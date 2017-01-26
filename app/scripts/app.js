f3a60f1d9a662a7918e87f7aa1667b2b9b27b80a
Meir Taffel <artgardening@gmail.com>
2017-01-26 17:31:14 +0200

    Complete: checkpoint 5 assign albumctrl. add controller for albumdata

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,25 +2,26 @@
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-            enabled: true,
-            requireBase: false
-        });
+                enabled: true,
+                requireBase: false
+            });
         
         $stateProvider
             .state('landing', {
-            url: '/',
-            controller: 'LandingCtrl as landing',
-            templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        })
-        .state('collection', {
-            url: '/collection',
-            controller: 'CollectionCtrl as collection',
-            templateUrl: '/templates/collection.html'
-        });
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                controller: 'AlbumCtrl as album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
+            });
 }
 
     angular
