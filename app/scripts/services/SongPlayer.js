96ffa13682c19bd610d1f724944dfc17d68a264f
Meir Taffel <artgardening@gmail.com>
2017-01-27 13:00:52 +0200

    Complete: checkpoint 7 assign playSong. add docs in SongPlayer.js.

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,10 +2,23 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+        /**
+        * @desc object that contains song data
+        * @type {Object}
+        */
         var currentSong = null;
+        
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
         
-
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -20,11 +33,20 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays song stored in currentBuzzObject sets `playing` logic to true
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
