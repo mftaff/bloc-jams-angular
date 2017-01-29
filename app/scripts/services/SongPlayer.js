bd39c141ee697cfe24cc35d3c510158c9a2e614c
Meir Taffel <artgardening@gmail.com>
2017-01-29 13:15:47 +0200

    Complete: checkpoint 8 assign next. add next button functionality. add dynamic song info to display on playbar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -21,8 +21,7 @@
         */
         var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong();
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -42,6 +41,11 @@
             currentBuzzObject.play();
             song.playing = true;
         };
+        
+        var stopSong = function() {
+            currentBuzzObject.stop();
+            SongPlayer.currentSong.playing = null;
+        }
         
         /**
         * @desc object that contains song data
@@ -75,8 +79,21 @@
             currentSongIndex--;
             
             if (currentSongIndex < 0) {
+                stopSong();
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if (currentSongIndex > currentAlbum.songs.length) {
                 currentBuzzObject.stop();
-                SongPlayer.currentSong.playing  = null;
+                SongPlayer.currentSong.playing  = null; 
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
